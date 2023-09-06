61201e70ce794a1fd68f040701b8aaef1dadabab
yazar-propel <yazararabaths@propelsoft.in>
2023-09-06 12:56:42 +0530

    alert updated

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="./css/Lato.css">
 <link rel="stylesheet" href="./css/font-awesome.min.css">
 <link rel="stylesheet" href="./css/sweetalert.css">
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="./js/sweetalert.js"></script>
 </a>
 </head>
 <body>
