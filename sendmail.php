ed5b9492484f0944899813216e900e844186f381
yazar-propel <yazararabaths@propelsoft.in>
2023-09-13 16:19:32 +0530

    mail updated

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 
-use PHPMailer\PHPMailer\PHPMailer;
+/*use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
@@ -47,18 +47,19 @@ try {
     //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
-echo json_encode($data);
+echo json_encode($data);*/
 
-/*$to = "yazar.vr46@gmail.com";
-$subject = "This is subject";
+$data = [];
+$to = "yazar.vr46@gmail.com";
+$subject = "Mail Received from BELMONT Website User";
 
 $message = "";
-if(isset($_POST['Name']))
-$message .= "<h1>This is ".$_POST['Name']."</h1>";
-if(isset($_POST['Message']))
-$message .= $_POST['Message']."<BR/>";
-if(isset($_POST['Email']))
-$message .= "Reply to: ". $_POST['Email'];
+if(isset($_POST['name']))
+$message .= "<h1>This is ".$_POST['name']."</h1>";
+if(isset($_POST['msg']))
+$message .= $_POST['msg']."<BR/>";
+if(isset($_POST['email']))
+$message .= "Reply to: ". $_POST['email'];
 
 $header = "From:yazarvale@gmail.com \r\n";
 $header .= "MIME-Version: 1.0\r\n";
@@ -67,18 +68,22 @@ $header .= "Content-type: text/html\r\n";
 $retval = mail ($to,$subject,$message,$header);
 
 if( $retval == true ) {
-echo "<script>Swal.fire({
-    title: 'Success!',
-    text: 'Mail sent successfully...!',
-    icon: 'success',
-    confirmButtonText: 'OK'
-});</script>";
+    $data['success'] = true;
+    $data['message'] = "<script>Swal.fire({
+                        title: 'Success!',
+                        text: 'Mail sent successfully...!',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                        });</script>";
 } else {
-echo "<script>Swal.fire({
-title: 'Error!',
-text: 'Message could not be sent...!',
-icon: 'error',
-confirmButtonText: 'OK'
-});</script>";
-}*/
+    $data['success'] = false;
+    $data['message'] = "<script>Swal.fire({
+                        title: 'Error!',
+                        text: 'Message could not be sent...!',
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                        });</script>";
+}
+
+echo json_encode($data);
 ?>
